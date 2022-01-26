fb4c24c3bba98af8fb6e383ef4dacb02d87ddc4c
Ourous <Ourous@users.noreply.github.com>
2022-01-26 19:26:31 +1100

    large speed improvements

--- main.cpp
+++ main.cpp
@@ -37,9 +37,9 @@ void compute_girth_stats(size_t p) {
     std::set<size_t> ones;
     // TODO: store single twos in different set to speed up subset checking
     std::set<std::set<size_t>> twos; // never need to store anything if a subset is already in here
-    // std::map<std::set<size_t>, size_t> others; // once we fill out 3s we can delete 2s and so-on
-    // std::map<std::set<size_t>, int> girths;
-    // std::map<int,std::set<std::set<size_t>>> girths;
+
+    std::vector<size_t> singles(p, 0); // a -> g
+
     std::vector<std::vector<std::vector<size_t>>> girths(p, std::vector<std::vector<size_t>>());
 
     for (size_t a = 0; a < p; a++) {
@@ -55,12 +55,14 @@ void compute_girth_stats(size_t p) {
             }
         }
         if (girth_is_one) {
-            ones.insert(a);
+            // ones.insert(a);
+            singles[a] = 1;
             // girths.insert({ { a }, 1 });
             // girths[1].push_back({ a });
         }
         else if (girth_maybe_two) {
-            twos.insert({ a });
+            // twos.insert({ a });
+            singles[a] = 2;
             // girths.insert({ { a }, 2 });
             // girths[2].push_back({ a });
         }
@@ -86,10 +88,8 @@ void compute_girth_stats(size_t p) {
         int g = p;
         for (size_t v = 0; v < p; v++) {
             g = std::min(g, bfs(m, v, g));
-            // auto girth = bfs(m, v, g);
-            // std::cout << "from " << v << ": " << girth << std::endl;
         }
-        // girths.insert({ { a }, g });
+        singles[a] = g;
         girths[g].push_back({ a });
 
         std::cout << a << ": " << g << std::endl;
@@ -105,6 +105,7 @@ void compute_girth_stats(size_t p) {
                 if (!combination[i]) continue;
                 coefficients.push_back(others[i]);
             }
+
             int g = 0;
             [&] {
                 for (; g < girths.size(); g++) {
@@ -115,45 +116,21 @@ void compute_girth_stats(size_t p) {
                     }
                 }
             }();
+
             if (g <= s) continue;
             if (g > s) {
-                std::cout << "g: " << g << ", s: " << s << std::endl;
+                // std::cout << "g: " << g << ", s: " << s << std::endl;
                 std::set<std::pair<size_t, size_t>> combined_function;
                 for (auto a : coefficients) {
                     // combined_function = set_merge_linear_time(quadratics[a], combined_function);
                     combined_function.insert(quadratics[a].begin(), quadratics[a].end());
                 }
 
-                // for g = 1, there is a subset with (s=1,g=1)
-                // for g = 2, there is a subset with (s<2,g=2)
-                // for g = 3, there is a subset with (s<3,g=3)
-                // for g = g`, there is a subset with (s<g`,g=g`)
-
-                // BIG IDEA: 
-
-                // for s = 3, if g < 3 we will know via subset
-                // for s = 4, if g < 4 we will know via subset
-                // for s = 5, if g < 5 we will know via subset
-
-                // once we retreive the initial girth via checking subsets
-                // if the initial g = s: that is the final girth
-
-                // so we only store subsets where g >= s
-
-                // I *think* we don't need to check where g == s and we can assume it remains the same
-                // TODO: CHECK THE ABOVE LINE; IMPLEMENT IF TRUE
-
-                // SECOND IDEA:
-
-                // we can remove single functions with g = N when s > N
-                // TODO: TEST WITH DATA ^
-
                 csr_matrix m(p, combined_function);
                 int initial_g = g;
-                for (size_t v = 0; v < p; v++) g = std::min(g, bfs(m, v, g));
+                for (size_t v = 0; v < p && g > s; v++) g = std::min(g, bfs(m, v, g));
                 if (initial_g > g) girths[g].push_back(coefficients);
             }
-            // girths.insert({ coefficients, g });
             for (auto i = coefficients.begin(); i != coefficients.end();) {
                 std::cout << *i;
                 if (i++ == coefficients.end()) break;
@@ -162,15 +139,45 @@ void compute_girth_stats(size_t p) {
             std::cout << ": " << g << std::endl;
 
         } while (std::next_permutation(combination.rbegin(), combination.rend()));
+
+        for (int a = 0; a < p; a++) {
+            if (singles[a] == s + 1) {
+                quadratics.erase(a);
+            }
+        }
     }
 }
+
+// for g = 1, there is a subset with (s=1,g=1)
+// for g = 2, there is a subset with (s<2,g=2)
+// for g = 3, there is a subset with (s<3,g=3)
+// for g = g`, there is a subset with (s<g`,g=g`)
+
+// BIG IDEA: 
+
+// for s = 3, if g < 3 we will know via subset
+// for s = 4, if g < 4 we will know via subset
+// for s = 5, if g < 5 we will know via subset
+
+// once we retreive the initial girth via checking subsets
+// if the initial g = s: that is the final girth
+
+// so we only store subsets where g >= s
+
+// I *think* we don't need to check where g == s and we can assume it remains the same
+// TODO: CHECK THE ABOVE LINE; IMPLEMENT IF TRUE
+
+// SECOND IDEA:
+
+// we can remove single functions with g = N when s > N
+// TODO: TEST WITH DATA ^
 
 int main(int, char**) {
     std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
 
-    size_t p = 101;
-    //97 = 17 seconds with twos, 5 seconds with full lookup
-    //
+    size_t p = 313;
+    //97 = 17 seconds with twos
+    //401 = 17 seconds with full lookup
 
     compute_girth_stats(p);
 
